a29a76cb3c34624aeb755d3ea77ce56746602262
dstuan02@gmail.com <dstuan02@gmail.com>
2023-02-17 14:34:17 +0700

    update controller

--- DemoTesterC1/src/main/java/com/dstuan/demotesterc1/PrimaryController.java
+++ DemoTesterC1/src/main/java/com/dstuan/demotesterc1/PrimaryController.java
@@ -7,6 +7,8 @@ public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        int a = 1;
+        if (a > 0)
+            App.setRoot("secondary");
     }
 }
